4cb338ebac6f54a96ae09fad92e7b3508c2a533d
zmjack <jack@zmland.com>
2019-12-09 14:23:24 +0800

    v0.0.6.1

---
+++ NStandard.Test/IO/SequenceInputStreamTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace NStandard.IO.Test
+{
+    public class SequenceInputStreamTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            using var stream = SequenceInputStream.Create(EnumerableEx.Concat(new[]
+            {
+                new MemoryStream("123".Bytes()),
+                new MemoryStream("456".Bytes()),
+                new MemoryStream("789".Bytes()),
+            }).ToArray());
+            using var reader = new StreamReader(stream);
+
+            Assert.Equal("123456789", reader.ReadToEnd());
+        }
+    }
+}
