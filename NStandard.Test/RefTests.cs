c09e9941e3c95ec66060d93f4590f477e6815de7
zmjack <jack@zmland.com>
2019-12-13 19:16:26 +0800

    v0.0.8

---
+++ NStandard.Test/RefTests.cs
@@ -0,0 +1,78 @@
+﻿using Xunit;
+
+namespace NStandard.Test
+{
+    public class RefTests
+    {
+        [Fact]
+        public void RRTest()
+        {
+            Ref<int> ra = Ref.Bind(0);
+            Ref<int> rb = Ref.Bind(0);
+
+            Assert.True(ra.Value == rb.Value);
+            Assert.False(ra.Value != rb.Value);
+
+            Assert.False(ra.RefValue == rb.RefValue);
+            Assert.True(ra.RefValue != rb.RefValue);
+
+            Assert.False(ra == rb);         // Compare as Ref<int>
+            Assert.True(ra != rb);          // Compare as Ref<int>
+
+            Assert.True(ra.Equals(rb));
+        }
+
+        [Fact]
+        public void RITest()
+        {
+            Ref<int> ri = Ref.Bind(0);
+            int i = 0;
+
+            Assert.True(ri.Value == i);
+            Assert.True(i == ri.Value);
+            Assert.False(ri.Value != i);
+            Assert.False(i != ri.Value);
+
+            Assert.False(ri.RefValue == (object)i);
+            Assert.False((object)i == ri.RefValue);
+            Assert.True(ri.RefValue != (object)i);
+            Assert.True((object)i != ri.RefValue);
+
+            Assert.False(ri == i);           // Compare as Ref<int> (left-side)
+            Assert.True(ri != i);            // Compare as Ref<int> (left-side)
+
+            Assert.True(i == ri);            // Compare as int
+            Assert.False(i != ri);           // Compare as int
+
+            Assert.True(ri.Equals(i));
+            Assert.True(i.Equals(ri));
+        }
+
+        [Fact]
+        public void RDTest()
+        {
+            Ref<double> rd = Ref.Bind(0d);
+            int i = 0;
+
+            Assert.True(rd.Value == i);
+            Assert.True(i == rd.Value);
+            Assert.False(rd.Value != i);
+            Assert.False(i != rd.Value);
+
+            Assert.False(rd.RefValue == (object)i);
+            Assert.False((object)i == rd.RefValue);
+            Assert.True(rd.RefValue != (object)i);
+            Assert.True((object)i != rd.RefValue);
+
+            Assert.False(rd == i);           // Compare as Ref<double> (left-side)
+            Assert.True(rd != i);            // Compare as Ref<double> (left-side)
+
+            Assert.True(i == rd);            // Compare as double
+            Assert.False(i != rd);           // Compare as double
+
+            Assert.False(rd.Equals(i));
+            Assert.False(i.Equals(rd));
+        }
+
+    }
+}
---
+++ NStandard/Ref.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NStandard
+{
+    public static class Ref
+    {
+        public static Ref<T> Bind<T>(T value) where T : struct => new Ref<T>(value);
+    }
+
+    public class Ref<T>
+        where T : struct
+    {
+        public object RefValue;
+        public T Value => (T)RefValue;
+
+        public Ref(T value) => RefValue = value;
+
+        public static bool operator ==(Ref<T> left, T right) => false;
+        public static bool operator !=(Ref<T> left, T right) => true;
+
+        public static bool operator ==(Ref<T> left, Ref<T> right) => left.RefValue == right.RefValue;
+        public static bool operator !=(Ref<T> left, Ref<T> right) => left.RefValue != right.RefValue;
+
+        public static implicit operator T(Ref<T> @this) => @this.Value;
+
+        public override bool Equals(object obj)
+        {
+            switch (obj)
+            {
+                case Ref<T> _obj: return Value.Equals(_obj.Value);
+                case T _obj: return Value.Equals(_obj);
+                default: return obj.Return(x => Value.Equals(x), x => false);
+            }
+        }
+
+        public override int GetHashCode() => RefValue.GetHashCode();
+        public override string ToString() => RefValue.ToString();
+    }
+}
