7967af1145b87df2e9096275fb0fea26b40f9b3c
zmjack <jack@zmland.com>
2020-02-16 21:27:08 +0800

    v0.2.12

---
+++ NStandard.Test/GuidEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NStandard.Test
+{
+    public class GuidExTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(Guid.Empty.ToString(), GuidEx.EMPTY_STRING);
+        }
+
+    }
+}
---
+++ NStandard/GuidEx.cs
@@ -0,0 +1,9 @@
+﻿
+namespace NStandard
+{
+    public static class GuidEx
+    {
+        public const string EMPTY_STRING = "00000000-0000-0000-0000-000000000000";
+
+    }
+}
