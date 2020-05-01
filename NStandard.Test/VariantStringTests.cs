a581e3017b298799558b4f55700d36b76b45402f
zmjack <jack@zmland.com>
2020-05-01 18:38:26 +0800

    v0.3.21

---
+++ NStandard.Test/VariantStringTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NStandard.Test
+{
+    public class VariantStringTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(new VariantString("true"));
+            Assert.False(new VariantString("false"));
+        }
+
+    }
+}
