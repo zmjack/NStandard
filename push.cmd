a862016dac31a455d1422bd22ee0d3c8b12b179c
zmjack <jack@zmland.com>
2020-05-11 20:53:41 +0800

    v0.4.0

--- push.cmd
+++ push.cmd
@@ -1,3 +1,3 @@
-nuget push "NStandard/bin/Release/NStandard.0.3.23.nupkg" -source nuget.org
-nuget push "NStandard.Expansion/bin/Release/NStandard.Expansion.0.3.0.nupkg" -source nuget.org
+nuget push "NStandard/bin/Release/NStandard.0.4.0.nupkg" -source nuget.org
+nuget push "NStandard.EncodingEx/bin/Release/NStandard.EncodingEx.0.4.0.nupkg" -source nuget.org
 pause
