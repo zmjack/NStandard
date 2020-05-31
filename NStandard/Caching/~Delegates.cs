ba864b4b8d54cd63e4d41406d95d4838396f2026
zmjack <jack@zmland.com>
2020-05-31 14:27:41 +0800

    v0.4.6

--- NStandard/Caching/~Delegates.cs
+++ NStandard/Caching/~Delegates.cs
@@ -2,6 +2,11 @@
 
 namespace NStandard.Caching
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cacheTime"></param>
+    /// <returns>Expiration Time</returns>
     public delegate DateTime UpdateCacheExpirationDelegate(DateTime cacheTime);
     public delegate void CacheUpdateEventDelegate<T>(DateTime cacheTime, T value);
 }
