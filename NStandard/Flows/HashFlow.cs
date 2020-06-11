547405da9180e15477cf41a4073457363fa414ff
zmjack <jack@zmland.com>
2020-06-12 01:39:53 +0800

    v0.4.10.1

---
+++ NStandard/Flows/HashFlow.cs
@@ -0,0 +1,10 @@
+﻿using NStandard.Security;
+
+namespace NStandard.Flows
+{
+    public static class HashFlow
+    {
+        public static IFlow<byte[], byte[]> Md5 = new Flow<byte[], byte[]>(Md5Provider.ComputeHash);
+        public static IFlow<byte[], byte[]> Sha1 = new Flow<byte[], byte[]>(Sha1Provider.ComputeHash);
+    }
+}
---
+++ NStandard/Security/~HashAlgorithm/Sha1Provider.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+
+namespace NStandard.Security
+{
+    public static class Sha1Provider
+    {
+        public static byte[] ComputeHash(Stream inputStream) => SHA1.Create().ComputeHash(inputStream);
+        public static byte[] ComputeHash(byte[] buffer, int offset, int count) => SHA1.Create().ComputeHash(buffer, offset, count);
+        public static byte[] ComputeHash(byte[] buffer) => SHA1.Create().ComputeHash(buffer);
+    }
+}
