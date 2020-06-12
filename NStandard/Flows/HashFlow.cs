cf092239b73385fface6ca52227d871f00fbe7b9
zmjack <jack@zmland.com>
2020-06-12 12:52:28 +0800

    v0.4.10

--- NStandard/Flows/HashFlow.cs
+++ NStandard/Flows/HashFlow.cs
@@ -4,7 +4,7 @@ namespace NStandard.Flows
 {
     public static class HashFlow
     {
-        public static IFlow<byte[], byte[]> Md5 = new Flow<byte[], byte[]>(Md5Provider.ComputeHash);
-        public static IFlow<byte[], byte[]> Sha1 = new Flow<byte[], byte[]>(Sha1Provider.ComputeHash);
+        public static IFlow<byte[], byte[]> MD5 = new Flow<byte[], byte[]>(Md5Provider.ComputeHash);
+        public static IFlow<byte[], byte[]> SHA1 = new Flow<byte[], byte[]>(Sha1Provider.ComputeHash);
     }
 }
