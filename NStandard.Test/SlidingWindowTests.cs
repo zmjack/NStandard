56b2a97e13dc831c888d9937606af9358b9f751e
zmjack <jack@zmland.com>
2020-08-16 03:42:33 +0800

    v0.5.8

---
+++ NStandard.Test/SlidingWindowTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NStandard.Test
+{
+    public class SlidingWindowTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var numbers = new[] { 100, 200, 300, 400 };
+            var result = SlidingWindow.Slide(numbers, 2).Max(w => w.Sum());
+            Assert.Equal(700, result);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var window = new SlidingWindow<int>(2);
+
+            var list1 = new List<int[]>();
+            var list2 = new List<int[]>();
+
+            var numbers = new[] { 11, 22, 33 };
+            for (int i = 0; i < 3; i++)
+            {
+                window.Fill(numbers[i]);
+
+                Assert.Equal(0 + 11 * i, window[-1]);
+                Assert.Equal(numbers[i], window[0]);
+
+                list1.Add(window.ToArray());
+                list2.Add(window[-1..1]);
+            }
+
+            var excepted = new[]
+            {
+                new [] { default, 11 },
+                new [] { 11, 22 },
+                new [] { 22, 33 },
+            };
+
+            Assert.Equal(excepted, list1.ToArray());
+            Assert.Equal(excepted, list2.ToArray());
+        }
+
+    }
+}
--- NStandard/SlidingWindow.cs
+++ NStandard/SlidingWindow.cs
@@ -1,13 +1,79 @@
-﻿namespace NStandard
-{
-    public class SlidingWindow<T>
-    {
-        public T[] Values { get; set; }
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
-        public SlidingWindow(T[] values)
+namespace NStandard
+{
+    public static class SlidingWindow
+    {
+        public static IEnumerable<SlidingWindow<T>> Slide<T>(IEnumerable<T> enumerable, int capacity)
         {
-            Values = values;
+            var window = new SlidingWindow<T>(capacity);
+            foreach (var item in enumerable)
+            {
+                window.Fill(item);
+                if (window.IsFilled) yield return window;
+            }
+        }
+    }
+
+    public class SlidingWindow<T> : ISliceLength<T[]>, IEnumerable<T>
+    {
+        private readonly T[] _store;
+        private readonly int _maxIndex;
+        private int _fillCount;
+        public int Length { get; private set; }
+        public bool IsFilled => _fillCount == Length;
+
+        public SlidingWindow(int capacity)
+        {
+            if (capacity < 2) throw new ArgumentException("The capacity must be greater than 2.", nameof(capacity));
+
+            _store = new T[capacity];
+            Length = capacity;
+            _maxIndex = capacity - 1;
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        public void Fill(T obj)
+        {
+            for (int i = 0; i < _maxIndex; i++)
+            {
+                _store[i] = _store[i + 1];
+            }
+            _store[_maxIndex] = obj;
+
+            if (_fillCount < Length) _fillCount++;
+        }
+
+        public T[] Slice(int start, int length)
+        {
+            if (start > 0 || length > Length) throw new ArgumentException("Non-positive number required.", nameof(start));
+
+            var ret = new T[length];
+            Array.Copy(_store, start + _maxIndex, ret, 0, length);
+            return ret;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var value in _store) yield return value;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            foreach (var value in _store) yield return value;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i > 0) throw new ArgumentException("Non-positive number required.", nameof(i));
+
+                return _store[i + _maxIndex];
+            }
+        }
     }
+
 }
