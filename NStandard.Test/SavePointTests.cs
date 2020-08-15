723e50c8d74a8af3519d7ecd4e05b7925793de58
zmjack <jack@zmland.com>
2020-08-16 00:35:24 +0800

    v0.5.7

---
+++ NStandard.Test/SavePointTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NStandard.Test
+{
+    public class SavePointTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var point = new SavePoint<int>(2);
+
+            var list1 = new List<int[]>();
+            var list2 = new List<int[]>();
+
+            var numbers = new[] { 11, 22, 33 };
+            for (int i = 0; i < 3; i++)
+            {
+                point.Save(numbers[i]);
+
+                Assert.Equal(0 + 11 * i, point[-1]);
+                Assert.Equal(numbers[i], point[0]);
+
+                list1.Add(point.ToArray());
+                list2.Add(point[-1..1]);
+            }
+
+            var excepted = new[]
+            {
+                new [] { default, 11 },
+                new [] { 11, 22 },
+                new [] { 22, 33 },
+            };
+
+            Assert.Equal(excepted, list1.ToArray());
+            Assert.Equal(excepted, list2.ToArray());
+        }
+
+    }
+}
---
+++ NStandard/SavePoint.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NStandard
+{
+    public class SavePoint<T> : ISliceLength<T[]>
+    {
+        private readonly T[] _store;
+        private readonly int _maxIndex;
+        public int Length { get; private set; }
+
+        public SavePoint(int capacity)
+        {
+            if (capacity < 2) throw new ArgumentException("The capacity must be greater than 2.", nameof(capacity));
+
+            _store = new T[capacity];
+            Length = capacity;
+            _maxIndex = capacity - 1;
+        }
+
+        public void Save(T obj)
+        {
+            for (int i = 0; i < _maxIndex; i++)
+            {
+                _store[i] = _store[i + 1];
+            }
+            _store[_maxIndex] = obj;
+        }
+
+        public T[] Slice(int start, int length)
+        {
+            if (start > 0 || length > Length) throw new ArgumentException("Non-positive number required.", nameof(start));
+
+            var ret = new T[length];
+            Array.Copy(_store, start + _maxIndex, ret, 0, length);
+            return ret;
+        }
+
+        public T[] ToArray()
+        {
+            var ret = new T[Length];
+            Array.Copy(_store, 0, ret, 0, Length);
+            return ret;
+        }
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i > 0) throw new ArgumentException("Non-positive number required.", nameof(i));
+
+                return _store[i + _maxIndex];
+            }
+        }
+    }
+
+}
