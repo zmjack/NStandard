f0ac8a6f3dac0a2899916143fd11479c5bf34c85
zmjack <jack@zmland.com>
2020-08-24 17:09:29 +0800

    v0.5.13

--- NStandard.Test/DpContainerTests - DpFib.cs
+++ NStandard.Test/DpContainerTests - DpFib.cs
@@ -14,9 +14,29 @@ namespace NStandard.Test
         }
 
         [Fact]
-        public void DpFibTest()
+        public void DpFibTest1()
         {
-            Assert.Equal(1836311903, new DpFib()[45]);
+            var fib = new DpFib();
+            Assert.Equal(1836311903, fib[45]);
+        }
+
+        public int BadFib(int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return BadFib(n - 1) + BadFib(n - 2);
+        }
+
+        public int Fib(DefaultDpContainer<int, int> dp, int n)
+        {
+            if (n == 0 || n == 1) return 1;
+            return dp[n - 1] + dp[n - 2];
+        }
+
+        [Fact]
+        public void DpFibTest2()
+        {
+            var fib = DpContainer.Create<int, int>(Fib);
+            Assert.Equal(1836311903, fib[45]);
         }
 
     }
--- NStandard/DpContainer.cs
+++ NStandard/DpContainer.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace NStandard
 {
+    public static class DpContainer
+    {
+        public static DefaultDpContainer<TIn, TOut> Create<TIn, TOut>(Func<DefaultDpContainer<TIn, TOut>, TIn, TOut> stateTransferFunc) => new DefaultDpContainer<TIn, TOut>(stateTransferFunc);
+    }
+
     public abstract class DpContainer<TIn, TOut> : Dictionary<TIn, TOut>
     {
         public abstract TOut StateTransfer(TIn param);
@@ -11,12 +17,23 @@ namespace NStandard
             get
             {
                 var @this = this as Dictionary<TIn, TOut>;
-                if (!ContainsKey(key))
-                    @this[key] = StateTransfer(key);
+                if (!ContainsKey(key)) @this[key] = StateTransfer(key);
                 return @this[key];
             }
             set => (this as Dictionary<TIn, TOut>)[key] = value;
         }
+    }
+
+    public class DefaultDpContainer<TIn, TOut> : DpContainer<TIn, TOut>
+    {
+        private readonly Func<DefaultDpContainer<TIn, TOut>, TIn, TOut> StateTransferFunc;
+
+        public DefaultDpContainer(Func<DefaultDpContainer<TIn, TOut>, TIn, TOut> stateTransferFunc)
+        {
+            StateTransferFunc = stateTransferFunc;
+        }
+
+        public override TOut StateTransfer(TIn param) => StateTransferFunc(this, param);
     }
 
 }
