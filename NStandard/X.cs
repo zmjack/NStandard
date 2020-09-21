e13816d90a2261b907cb45bb4d88551d034259cc
zmjack <jack@zmland.com>
2020-09-21 16:52:11 +0800

    v0.5.17

---
+++ NStandard/X.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NStandard
+{
+    public static class X
+    {
+        public static T Create<T>(Func<T> create)
+        {
+            return create();
+        }
+
+    }
+}
