b54ff162372b415b4652da0871b4a4a4de71c8dd
zmjack <jack@zmland.com>
2021-04-23 01:22:28 +0800

    v0.7.0

--- push.cmd
+++ push.cmd
@@ -1,4 +1,4 @@
 nuget push "NStandard/bin/Release/NStandard.0.7.0.nupkg" -source nuget.org
-nuget push "NStandard.Runtime/bin/Release/NStandard.Runtime.0.70.nupkg" -source nuget.org
+nuget push "NStandard.Runtime/bin/Release/NStandard.Runtime.0.7.0.nupkg" -source nuget.org
 nuget push "NStandard.Encoding/bin/Release/NStandard.Encoding.0.5.0.nupkg" -source nuget.org
 pause
