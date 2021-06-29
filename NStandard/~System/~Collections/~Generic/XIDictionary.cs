a49349ae3ff64d9f39fa3ec1f0ef17f82775788f
zmjack <jack@zmland.com>
2021-06-30 05:52:39 +0800

    v0.8.6

--- NStandard/~System/~Collections/~Generic/XIDictionary.cs
+++ NStandard/~System/~Collections/~Generic/XIDictionary.cs
@@ -4,7 +4,7 @@ namespace NStandard
 {
     public static class XIDictionary
     {
-        public static TValue GetValueOrDefault<TKey, TValue>(IDictionary<TKey, TValue> @this, TKey key) where TKey : notnull
+        public static TValue GetValueOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> @this, TKey key) where TKey : notnull
         {
             if (@this.TryGetValue(key, out var value)) return value;
             else return default;
