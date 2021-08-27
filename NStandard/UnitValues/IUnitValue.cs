f6c555cf9bf916ae836c9457d8e377338def6475
zmjack <jack@zmland.com>
2021-08-27 17:56:49 +0800

    v0.8.18

--- NStandard/UnitValues/IUnitValue.cs
+++ NStandard/UnitValues/IUnitValue.cs
@@ -7,4 +7,17 @@
         double Value { get; }
         string GetDefaultUnit();
     }
+
+}
+
+namespace NStandard
+{
+    public static class XIUnitValue
+    {
+        public static TSelf WithUnit<TSelf>(this TSelf @this, string unit) where TSelf : struct, UnitValues.IUnitValue
+        {
+            @this.Unit = unit;
+            return @this;
+        }
+    }
 }
