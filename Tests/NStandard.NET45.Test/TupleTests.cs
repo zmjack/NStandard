bc76a4849242df7c29dc23255d27b098b62d91d7
zmjack <jack@zmland.com>
2022-04-09 00:42:34 +0800

    v0.8.45

---
+++ Tests/NStandard.NET45.Test/TupleTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NStandard.NET45.Test
+{
+    [TestClass]
+    public class TupleTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var arr = new[]
+            {
+                Tuple.Create(3, 2),
+                Tuple.Create(3, 1),
+            };
+            var ordered = arr.OrderBy(x => x).ToArray();
+            Assert.AreEqual(1, ordered[0].Item2);
+            Assert.AreEqual(2, ordered[1].Item2);
+        }
+    }
+}
