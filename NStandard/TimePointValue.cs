775093502459c6b72237104cb3e99a9d683a6445
zmjack <jack@zmland.com>
2022-06-16 05:37:59 +0800

    v0.9.11

---
+++ NStandard/TimePointValue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NStandard
+{
+    public static class TimePointValue
+    {
+        public static TimePointValue<TValue> Create<TValue>(TValue value) => new(value);
+    }
+
+    public struct TimePointValue<TValue>
+    {
+        public TimePointValue() : this(default) { }
+        public TimePointValue(TValue value)
+        {
+            TimePoint = DateTime.Now;
+            _value = value;
+        }
+
+        public DateTime TimePoint { get; private set; }
+        public TimeSpan TimeSpan => DateTime.Now - TimePoint;
+
+        private TValue _value;
+        public TValue Value
+        {
+            get => _value;
+            set
+            {
+                if (!_value.Equals(value))
+                {
+                    _value = value;
+                    TimePoint = DateTime.Now;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/NStandard.Test/TimePointValueTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Xunit;
+
+namespace NStandard.Test
+{
+    public class TimePointValueTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var pointValue = new TimePointValue<int>(1);
+
+            Thread.Sleep(200);
+            pointValue.Value = 1;
+            Assert.True(pointValue.TimeSpan.TotalMilliseconds > 200);
+
+            Thread.Sleep(200);
+            pointValue.Value = 2;
+            Assert.True(pointValue.TimeSpan.TotalMilliseconds < 200);
+        }
+
+    }
+}
