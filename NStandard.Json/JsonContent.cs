ec6a65e8a899f9912963db24a234d0d1820ec962
Jack.Tan <195587293@qq.com>
2022-09-05 11:51:34 +0800

    v0.10.1

---
+++ NStandard.Json/JsonContent.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace NStandard.Json
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object obj) : base(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json") { }
+        public JsonContent(object obj, JsonSerializerOptions options) : base(JsonSerializer.Serialize(obj, options), Encoding.UTF8, "application/json") { }
+        public JsonContent(object obj, JsonSerializerOptions options, Encoding encoding) : base(JsonSerializer.Serialize(obj, options), encoding, "application/json") { }
+    }
+}
--- push.cmd
+++ push.cmd
@@ -1,6 +1,6 @@
 nuget push "NStandard/bin/Release/NStandard.0.10.1.nupkg" -source nuget.org
 nuget push "NStandard.Runtime/bin/Release/NStandard.Runtime.0.10.1.nupkg" -source nuget.org
 nuget push "NStandard.Encoding/bin/Release/NStandard.Encoding.0.7.0.nupkg" -source nuget.org
-nuget push "NStandard.Json/bin/Release/NStandard.Json.0.3.0.nupkg" -source nuget.org
-nuget push "NStandard.Json.Net/bin/Release/NStandard.Json.Net.0.3.0.nupkg" -source nuget.org
+nuget push "NStandard.Json/bin/Release/NStandard.Json.0.3.5.nupkg" -source nuget.org
+nuget push "NStandard.Json.Net/bin/Release/NStandard.Json.Net.0.3.5.nupkg" -source nuget.org
 pause
