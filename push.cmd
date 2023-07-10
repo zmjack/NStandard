dee02d00b614563e029774513e706f487394f8ec
zmjack <jack@zmland.com>
2023-07-10 08:46:54 +0800

    NStandard.0.35.0

--- push.cmd
+++ push.cmd
@@ -1,6 +1,6 @@
 nuget push "NStandard/bin/Release/NStandard.0.35.0.nupkg" -source nuget.org
-nuget push "NStandard.Runtime/bin/Release/NStandard.Runtime.0.20.0.nupkg" -source nuget.org
+nuget push "NStandard.Runtime/bin/Release/NStandard.Runtime.0.21.0.nupkg" -source nuget.org
 nuget push "NStandard.Encoding/bin/Release/NStandard.Encoding.0.8.0.nupkg" -source nuget.org
-nuget push "NStandard.Json/bin/Release/NStandard.Json.0.5.0.nupkg" -source nuget.org
-nuget push "NStandard.Json.Net/bin/Release/NStandard.Json.Net.0.5.0.nupkg" -source nuget.org
+nuget push "NStandard.Json/bin/Release/NStandard.Json.0.6.0.nupkg" -source nuget.org
+nuget push "NStandard.Json.Net/bin/Release/NStandard.Json.Net.0.6.0.nupkg" -source nuget.org
 pause
