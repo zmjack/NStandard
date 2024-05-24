1ab2da766d9d473668c18462cbb5a0b2ee33ef2e
zmjack <jack@zmland.com>
2024-05-24 12:48:35 +0800

    NStandard.0.64.1

--- NStandard/Text/Json/JsonImplConverterAttribute.cs
+++ NStandard/Text/Json/JsonImplConverterAttribute.cs
@@ -1,13 +1,25 @@
-﻿#if NET5_0_OR_GREATER
-using System;
+﻿using System;
+#if NET5_0_OR_GREATER
 using System.Text.Json.Serialization;
 
 namespace NStandard.Text.Json;
 
-[AttributeUsage(AttributeTargets.Interface, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
 public class JsonImplConverterAttribute<T> : JsonConverterAttribute
 {
     public JsonImplConverterAttribute() : base(typeof(JsonImplConverter<T>))
+    {
+    }
+}
+#else
+/// <summary>
+/// Must be manually configured based on the actual serializer used.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+public class JsonImplConverterAttribute<T> : Attribute
+{
+    public JsonImplConverterAttribute()
     {
     }
 }
