666154ec04a4a7a2d77b48a9d6c5896aad1f0a5f
zmjack <jack@zmland.com>
2024-07-15 13:10:00 +0800

    NStandard.0.75.2

--- push.cmd
+++ push.cmd
@@ -1,4 +1,5 @@
 nuget push "NStandard/bin/Release/NStandard.0.75.2.nupkg" -source nuget.org
+nuget push "NStandard.Analyzer/bin/Release/NStandard.Analyzer.0.0.1-alpha.nupkg" -source nuget.org
 nuget push "NStandard.Runtime/bin/Release/NStandard.Runtime.1.8.0.nupkg" -source nuget.org
 nuget push "NStandard.Encoding/bin/Release/NStandard.Encoding.0.8.5.nupkg" -source nuget.org
 nuget push "NStandard.Json/bin/Release/NStandard.Json.0.8.0.nupkg" -source nuget.org
